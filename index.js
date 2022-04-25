99d006a5d0b900064a1741e2c685aeefb65e05fb
Emine Çığ <emine.cig67@gmail.com>
2022-04-25 16:53:29 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+// distanceFromHqInBlocks
+function distanceFromHqInBlocks(blockNumber){
+    if(blockNumber>42)
+        return blockNumber-42
+    
+    else
+        return 42-blockNumber;
+    
+}
+//distanceFromHqInFeet
+function distanceFromHqInFeet(blockNumber){
+    return   distanceFromHqInBlocks (blockNumber) *264;
+
+}
+//distanceTravelledInFeet
+function distanceTravelledInFeet(start,dest,){
+    if(start<dest)
+        return (dest-start)*264;
+
+    else       
+        return (start-dest)*264;
+
+}
+//calculatesFarePrice
+function calculatesFarePrice(start ,dest){
+    const dist= distanceTravelledInFeet(start,dest);
+
+    if(dist <=400)
+        return 0;
+    else if( dist > 400 && dist <= 2000)
+        return  (dist-400)* 0.02;
+
+     else if( dist > 2000 && dist < 2500)
+        return 25;
+    else
+        return 'cannot travel that far'        ;
+}
